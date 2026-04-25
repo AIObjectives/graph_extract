4cfc5d8f8a50249e4a8d7ed568161de2560f758a
aleshinskaya <anna.leshinskaya@gmail.com>
2026-04-24 20:04:27 -0700

    cleaned up unused prompts

--- src/prompts.py
+++ src/prompts.py
@@ -10,7 +10,7 @@ def get_beings(this_scenario, this_act):
         
 def get_events(this_scenario, this_act):
 
-    system_prompt_content = f"""You are an expert at understanding human situations. A human named Ziv has described a scenario and how they decided to act. Your task is to identify all the outcomes that will probably occur as a result of Ziv's action decision, especially any impacts on sentient beings involved. Please state each outcome as the most simple, immediately occuring result, and not chains of events.  Name the outcome only, without any qualifiers like "maybe" or "may". Describe the outcomes referring to Ziv using their name, not pronouns. Return a json object with key:value pair of "results": list of events. Please be diligent, complete, and succinct in your response."""
+    system_prompt_content = f"""You are an expert at understanding human situations. A human named Ziv has described a scenario and how they decided to act. Your task is to identify all the outcomes that will probably occur as a result of Ziv's action decision, especially any impacts on sentient beings involved. Please state each outcome as the most simple, immediately occuring result, and not chains of events. Do not include any events that are simply re-statements of the action that was taken. Name the outcome only, without any qualifiers like "maybe" or "may". Describe the outcomes referring to Ziv using their name, not pronouns. Return a json object with key:value pair of "results": list of events. Please be diligent, complete, and succinct in your response."""
     # print(system_prompt_content)
     user_prompt_content = f'Here is Ziv\'s scenario: {this_scenario}. Ziv decided to {this_act}. What outcomes are likely to arise a result of Ziv\'s decision?'
 
@@ -18,7 +18,7 @@ def get_events(this_scenario, this_act):
            
 def convert_Ziv_I(orig_text):
    
-  this_resp=utils.get_response_dict("You are an expert in English grammar. Rewrite the following text so that it is written in the first person perspective instead of in the third person about Ziv, replacing each reference to Ziv by name or pronoun with the correct first-person pronoun (I, me, or my).  If Ziv is not explicitly mentioned, return the original text. Return a json with key name 'converted_text' whose value contains the converted text.", orig_text)
+  this_resp = utils.get_response_dict("You are an expert in English grammar. Rewrite the following text so that it is written in the first person perspective instead of in the third person about Ziv, replacing each reference to Ziv by name or pronoun with the correct first-person pronoun (I, me, or my).  If Ziv is not explicitly mentioned, return the original text. Return a json with key name 'converted_text' whose value contains the converted text.", orig_text)
 
   new_text = this_resp['converted_text']
 
@@ -32,17 +32,10 @@ def find_semantic_match(item,list1):
             
   return utils.get_response_dict(system_prompt_content, user_prompt_content)
 
-# def convert_I_Ziv_item(old_item):
-   
-#   this_resp=utils.get_response_dict("You are an expert in English grammar. Take this phrase and replace each mention of the first-person pronouns I, me, or my with the name Ziv or Ziv's. If I, me, or my is not present, return the original text. Return a json with key 'converted_text' containing only the converted text.", old_item)
-
-#   new_sentence = this_resp['converted_text']
-
-#   return(new_sentence)
   
 def convert_I_Ziv(orig_text):
     
-    this_resp=utils.get_response_dict("You are an expert in English grammar. Rewrite the following text so that it is written from the perspective of a character name Ziv in third person instead of being written in the first person. Replace every instance of the first person pronoun (I, me, my, etc) with either the name Ziv or the pronouns they, their, them, etc. Return a json object with key 'converted_text' containing only the converted text.", orig_text)
+    this_resp = utils.get_response_dict("You are an expert in English grammar. Rewrite the following text so that it is written from the perspective of a character name Ziv in third person instead of being written in the first person. Replace every instance of the first person pronoun (I, me, my, etc) with either the name Ziv or the pronouns they, their, them, etc. Return a json object with key 'converted_text' containing only the converted text.", orig_text)
 
     new_text = this_resp['converted_text']
 
@@ -55,62 +48,6 @@ def convert_lower(sentence):
     new_s = new_s.replace("ziv's","Ziv's")
 
     return new_s
-
-# def score_action_deontology(this_act):
-
-#    system_prompt_content = f"""According to deontological moral theories, the morality of a choice is evaluated based not on the states of affairs those choices bring about, but on the nature of the action itself. These theories hold that some choices cannot be justified by their effects — that no matter how morally good their consequences, some choices are morally forbidden. Instead, what makes a choice right is its conformity with a moral norm. For example, deontological moral theories consider the following moral norms: it is never justified to cause harm or suffering to anyone; that some rules should never be broken, even if breaking them allows for a greater good, and that some principles are universal: they do not depend on circumstances; and that a person’s life is sacred, and killing is always wrong. Consider the following action and rate how well it conforms to deontic moral norms, using a scale from -100 to 100, where -100 indicates that this action is the most extremely deontological wrong action possible, and violates deontic norms in a severe way, and +100 indicates that this action is extremely morally right and conforms to moral norms in a very strong way. A rating of 0 indicates that the action is morally neutral in terms of conformity with moral norms. Please return a json object with key:value pair of "score": rating."""
-
-
-#    user_prompt_content = f"Please rate this action: {this_act}"     
-#    print(system_prompt_content, user_prompt_content)
-   
-# #    return utils.get_response_dict(system_prompt_content, user_prompt_content)
-
-# def score_action_simple(this_act):
-   
-#    system_prompt_content = f"""You are an expert judge of the moral virtues of different actions. Moral actions are those which humans consider virtuous, that consider others' wellbeing and happiness, and that are guided by principles of ethics. You will be given a sentence describing an action. You must rate thus action in terms of its moral virtue. Respond with a rating from -100 to 100 for this action, where -100 is the most morally wrong action you can possibly think of, and 100 is the most morally virtuous action you can possibly think of. 0 indicates a morally neutral action. Please return a json object with key:value pair of "score": rating."""
-
-#    user_prompt_content = f"Please rate this action: {this_act}"     
-#    print(system_prompt_content, user_prompt_content)
-
-#    return utils.get_response_dict(system_prompt_content, user_prompt_content)
-
-
-# # pass list of values and score their importance
-# def score_values(this_scenario, this_act, values_list):
-   
-#    system_prompt_content = f"""You are an expert on human values and actions. The user will share a situation and an action they took, plus a list of values and anti-values that the action might have exhibited. Please rate to what extent the action is characterized by each value or anti-value. Use a scale of 0 to 100, where 0 indicates that this value or anti-value does not characterize this action, and 100 indicates that it very much characterizes this action. Return a json object with each value as a key and your rating as a value."""
-
-#    user_prompt_content = f"Here is my situation. {this_scenario} My action is to {this_act} To what extent is this action characterized by these values and anti-values? {values_list}"     
-
-
-
-#    return utils.get_response_dict(system_prompt_content, user_prompt_content)
-
-
-#    system_prompt_content = f"""You are an expert on what humans value and don't value. The user will share an action they chose to take in a situation. Your task is to identify the values and virtues that the user exhibits by taking this action. Return a json object called 'values' listing the values and nothing more."""
-          
-#     user_prompt_content = f"""Here is my scenario. {this_scenario} My action is to {this_act} List the virtues and values of this action."""
-
-# def get_value_positive(this_scenario, this_act):
-
-#     system_prompt_content = f"""You are an expert on human values. The user will share a situation and an action they decided to take. Identify the most important positive values and virtues that characterize this action. Return a json object called 'values' listing the values and nothing more."""
-          
-#     user_prompt_content = f"""Here is my scenario. {this_scenario} My action is to {this_act} List the most important values and virtues exhibited by this action."""
-
-   
-
-#     return utils.get_response_dict(system_prompt_content, user_prompt_content)
-
-# def get_value_negative(this_scenario, this_act):
-
-#     system_prompt_content = f"""You are an expert on human vices. The user will share a situation and an action they decided to take. Identify the most important anti-values and vices that characterize this action. Return a json object called 'anti-values' listing the vices and nothing more."""
-          
-#     user_prompt_content = f"""Here is my scenario. {this_scenario}. My action is to {this_act} List the most important vices exhibited by this action."""
-         
-
-#     return utils.get_response_dict(system_prompt_content, user_prompt_content)
-
 
 
 def get_impacts_Ziv_single(this_scenario, this_act, this_event, this_being):
@@ -148,17 +85,6 @@ def get_impacts_Ziv_multi(this_scenario, this_act, this_event, these_beings):
         system_prompt_content = f"You are a helpful assistant who is an expert at understanding human situations. The following scenario is context for the user's question. {this_scenario} {this_act}  End of scenario. Consider the possible outcome that {convert_lower(this_event)} Please rate how this outcome would impact each character listed by the user. Use a scale from -100 to +100, where -100 indicates that the outcome will cause the character substantial harm or cost, and +100 indicates that it will cause the character substantial benefit. Return a json object called 'results' with a key:value pair for being: rating."
 
         user_prompt_content = f'Consider the possibility that {convert_lower(this_event)} Without considering any further consequences of this event, how would this event directly impact each of these characters: {these_beings}?'
-
-        # print(system_prompt_content, user_prompt_content)
-        return utils.get_response_dict(system_prompt_content, user_prompt_content)
-
-
-
-def get_impacts_Ziv_noscenario(this_scenario, this_act, this_event, these_beings):
-
-        system_prompt_content = f'You are a helpful assistant who is an expert at understanding human situations. You will be given a description of an event and asked to evaluate how this event on its own (without considering any further consequences) is likely to impact the listed characters. Rate the direct impact on each character using a scale from -10 to +10, where a rating of -10 indicates that the outcome will directly cause the character substantial harm, while a rating of +10 indicates that it will directly cause the character substantial benefit. Please evaluate only the immediate, direct impact of the event on its own, without considering any further consequences or outcomes downstream. If you are not sure that the outcome will immediately and directly affect the character very much, return a rating of 0 or close to 0. Return a json object called "results" with a key:value pair for being: rating.'
-
-        user_prompt_content = f'Consider the event that {convert_lower(this_event)} Without considering any further consequences of this event, how does this event by itself directly impact each of these characters: {these_beings}?'
 
         # print(system_prompt_content, user_prompt_content)
         return utils.get_response_dict(system_prompt_content, user_prompt_content)
@@ -216,6 +142,73 @@ def get_being_links_Ziv_only_know(this_scenario, this_act, this_event, this_bein
 
 
 
+
+# def get_impacts_Ziv_noscenario(this_scenario, this_act, this_event, these_beings):
+
+#         system_prompt_content = f'You are a helpful assistant who is an expert at understanding human situations. You will be given a description of an event and asked to evaluate how this event on its own (without considering any further consequences) is likely to impact the listed characters. Rate the direct impact on each character using a scale from -10 to +10, where a rating of -10 indicates that the outcome will directly cause the character substantial harm, while a rating of +10 indicates that it will directly cause the character substantial benefit. Please evaluate only the immediate, direct impact of the event on its own, without considering any further consequences or outcomes downstream. If you are not sure that the outcome will immediately and directly affect the character very much, return a rating of 0 or close to 0. Return a json object called "results" with a key:value pair for being: rating.'
+
+#         user_prompt_content = f'Consider the event that {convert_lower(this_event)} Without considering any further consequences of this event, how does this event by itself directly impact each of these characters: {these_beings}?'
+
+#         # print(system_prompt_content, user_prompt_content)
+#         return utils.get_response_dict(system_prompt_content, user_prompt_content)
+
+
+
+
+# def score_action_deontology(this_act):
+
+#    system_prompt_content = f"""According to deontological moral theories, the morality of a choice is evaluated based not on the states of affairs those choices bring about, but on the nature of the action itself. These theories hold that some choices cannot be justified by their effects — that no matter how morally good their consequences, some choices are morally forbidden. Instead, what makes a choice right is its conformity with a moral norm. For example, deontological moral theories consider the following moral norms: it is never justified to cause harm or suffering to anyone; that some rules should never be broken, even if breaking them allows for a greater good, and that some principles are universal: they do not depend on circumstances; and that a person’s life is sacred, and killing is always wrong. Consider the following action and rate how well it conforms to deontic moral norms, using a scale from -100 to 100, where -100 indicates that this action is the most extremely deontological wrong action possible, and violates deontic norms in a severe way, and +100 indicates that this action is extremely morally right and conforms to moral norms in a very strong way. A rating of 0 indicates that the action is morally neutral in terms of conformity with moral norms. Please return a json object with key:value pair of "score": rating."""
+
+
+#    user_prompt_content = f"Please rate this action: {this_act}"     
+#    print(system_prompt_content, user_prompt_content)
+   
+# #    return utils.get_response_dict(system_prompt_content, user_prompt_content)
+
+# def score_action_simple(this_act):
+   
+#    system_prompt_content = f"""You are an expert judge of the moral virtues of different actions. Moral actions are those which humans consider virtuous, that consider others' wellbeing and happiness, and that are guided by principles of ethics. You will be given a sentence describing an action. You must rate thus action in terms of its moral virtue. Respond with a rating from -100 to 100 for this action, where -100 is the most morally wrong action you can possibly think of, and 100 is the most morally virtuous action you can possibly think of. 0 indicates a morally neutral action. Please return a json object with key:value pair of "score": rating."""
+
+#    user_prompt_content = f"Please rate this action: {this_act}"     
+#    print(system_prompt_content, user_prompt_content)
+
+#    return utils.get_response_dict(system_prompt_content, user_prompt_content)
+
+
+# # pass list of values and score their importance
+# def score_values(this_scenario, this_act, values_list):
+   
+#    system_prompt_content = f"""You are an expert on human values and actions. The user will share a situation and an action they took, plus a list of values and anti-values that the action might have exhibited. Please rate to what extent the action is characterized by each value or anti-value. Use a scale of 0 to 100, where 0 indicates that this value or anti-value does not characterize this action, and 100 indicates that it very much characterizes this action. Return a json object with each value as a key and your rating as a value."""
+
+#    user_prompt_content = f"Here is my situation. {this_scenario} My action is to {this_act} To what extent is this action characterized by these values and anti-values? {values_list}"     
+
+
+
+#    return utils.get_response_dict(system_prompt_content, user_prompt_content)
+
+
+#    system_prompt_content = f"""You are an expert on what humans value and don't value. The user will share an action they chose to take in a situation. Your task is to identify the values and virtues that the user exhibits by taking this action. Return a json object called 'values' listing the values and nothing more."""
+          
+#     user_prompt_content = f"""Here is my scenario. {this_scenario} My action is to {this_act} List the virtues and values of this action."""
+
+# def get_value_positive(this_scenario, this_act):
+
+#     system_prompt_content = f"""You are an expert on human values. The user will share a situation and an action they decided to take. Identify the most important positive values and virtues that characterize this action. Return a json object called 'values' listing the values and nothing more."""
+          
+#     user_prompt_content = f"""Here is my scenario. {this_scenario} My action is to {this_act} List the most important values and virtues exhibited by this action."""
+
+   
+
+#     return utils.get_response_dict(system_prompt_content, user_prompt_content)
+
+# def get_value_negative(this_scenario, this_act):
+
+#     system_prompt_content = f"""You are an expert on human vices. The user will share a situation and an action they decided to take. Identify the most important anti-values and vices that characterize this action. Return a json object called 'anti-values' listing the vices and nothing more."""
+          
+#     user_prompt_content = f"""Here is my scenario. {this_scenario}. My action is to {this_act} List the most important vices exhibited by this action."""
+         
+
+#     return utils.get_response_dict(system_prompt_content, user_prompt_content)
 
 
 
