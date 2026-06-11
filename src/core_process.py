49aa35ad990a592aa6d858f76c9a6ea7a4fa515b
aleshinskaya <anna.leshinskaya@gmail.com>
2026-06-11 16:34:27 -0700

    bug fixes for api error 431

--- src/core_process.py
+++ src/core_process.py
@@ -20,6 +20,7 @@ import src.prompts as prompts
 import src.node as node
 import src.utils as utils
 import src.moral_projection as moral_projection
+import src.util_projection as util_projection
 importlib.reload(get_emb_distances)
 importlib.reload(prompts)
 importlib.reload(node)
@@ -121,7 +122,12 @@ def process_values_minimal(this_act):
  
   return this_score
 
-
+def process_util_minimal(this_outcome):
+   
+  resp = util_projection.main([this_outcome])
+  this_score = round(resp['projection'].iloc[0]*1000, 0)
+ 
+  return this_score
 
 def process_outcomes(this_scenario, this_act):   
 
@@ -129,7 +135,7 @@ def process_outcomes(this_scenario, this_act):
   events = prompts.get_events(this_scenario, this_act)
   events_Ziv= events['results']
   # remove overly similar outcomes
-  events_Ziv=get_emb_distances.threshold_by_sim(events_Ziv,.06, CONFIG['OAI'])
+  events_Ziv = get_emb_distances.threshold_by_sim(events_Ziv, .06, CONFIG.get('OAI', ''))
   
   #replace Ziv with first person pronoun.        
   events_I = [prompts.convert_Ziv_I(x) if x.find("Ziv")>-1 else x for x in events_Ziv]   
